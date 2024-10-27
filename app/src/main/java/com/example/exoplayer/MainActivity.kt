cf1161fd6dd4e6d9f5fd8cc8c6dc0345d36f33ad
MfrankUg <muhindofrank2001@gmail.com>
2024-10-27 23:54:20 +0300

    video player

---
+++ app/src/main/java/com/example/exoplayer/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.exoplayer
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.MediaController
+import android.widget.VideoView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val videoView = findViewById<VideoView>(R.id.xml_video_view)
+        val packageName = "android.resource://" + getPackageName() + "/" + R.raw.trial
+        val uri = Uri.parse(packageName)
+        videoView.setVideoURI(uri)
+
+        // video controller variables
+        val mediaController = MediaController(this)
+        videoView.setMediaController(mediaController)
+    }
+}
